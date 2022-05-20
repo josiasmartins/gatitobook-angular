08ce442245322dbfa282ae945d299488c59d6bb8
Caitano <josiasmartinscaitano@gmail.com>
2022-05-20 20:38:15 -0300

    feat: exprtado o componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HomeModule } from './home/home.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
