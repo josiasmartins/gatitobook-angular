a4190948736a256f540b179b9d621364720a9e48
Caitano <josiasmartinscaitano@gmail.com>
2022-05-20 21:45:29 -0300

    feat: formulario Template-Driven

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  usuario = 'ibag';
+  senha = '';
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    console.log(this.usuario);
+    console.log(this.senha);
   }
 
 }
