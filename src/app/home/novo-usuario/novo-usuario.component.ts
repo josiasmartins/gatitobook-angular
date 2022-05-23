ecea924ae643970aa182da9c249a33096a1f6ba3
Caitano <josiasmartinscaitano@gmail.com>
2022-05-23 20:11:38 -0300

    feat: adicioando validação customizada

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -2,7 +2,7 @@ import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
-
+import { minusculoValidator } from './minusculo.validator'
 @Component({
   selector: 'app-novo-usuario',
   templateUrl: './novo-usuario.component.html',
@@ -27,7 +27,9 @@ export class NovoUsuarioComponent implements OnInit {
         Validators.required,
         Validators.minLength(4)
       ]],
-      userName: [''],
+      userName: ['', [
+        minusculoValidator
+      ]],
       password: ['']
     });
   }
