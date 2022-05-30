b728bc78506c54c544075980956e6f5a74224700
Caitano <josiasmartinscaitano@gmail.com>
2022-05-30 12:55:55 -0300

    feat: adicioando o servico de authentication

--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { UsuarioService } from './usuario/usuario.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +13,24 @@ export class AuthenticationService {
    * service
    *  implementa a regra do neocio
    */
-  constructor(private httpClient: HttpClient) { }
+  constructor(
+    private httpClient: HttpClient,
+    private usuarioService: UsuarioService
+  ) { }
 
   // Observable: parecido com a promise do javascript
-  autenticar(usuario: string, senha: string): Observable<any> {
+  autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
     return this.httpClient.post('http://localhost:3000/user/login', {
       userName: usuario,
       password: senha,
-    })
+    },
+    {observe: 'response' }).pipe(
+      // tap: somente fazendo a operação
+      tap((res) => {
+        const authToken = res.headers.get('x-access-token') ?? '';
+        this.usuarioService.salvaToken(authToken);
+      })
+    )
   }
 
 }
