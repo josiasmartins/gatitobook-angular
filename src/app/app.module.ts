70a34215222e749b415e97c2d37be87224ba350f
Caitano <josiasmartinscaitano@gmail.com>
2022-05-30 15:03:16 -0300

    fix: corrigido o import do rodape no app.component.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { RodapeComponent } from './components/rodape/rodape.component';
 import { CabecalhoModule } from './components/cabecalho/cabecalho.module';
 import { RodapeModule } from './components/rodape/rodape.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RodapeComponent
   ],
   imports: [
     BrowserModule,
